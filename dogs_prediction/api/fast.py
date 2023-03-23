bba0e92014acdd786c9b55e9b898cf4a8b419927
Steven Huth <stevenhuth@tutanota.com>
2023-03-23 11:49:58 +0100

    updated fast.api

--- dogs_prediction/api/fast.py
+++ dogs_prediction/api/fast.py
@@ -1,5 +1,31 @@
-from dogs_prediction.DL_logic.predict import load_latest_model()
+from dogs_prediction.DL_logic import predict
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+#iniate api
 app = FastAPI()
+
+# Optional, good practice for dev purposes. Allow all middlewares
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+# preload the model
+app.state.model = predict.load_latest_model()
+
+# add predict endpoint
+@app.get("/predict")
+def prediction(url: str):
+    model = app.state.model
+    predict.predict_breed(url, model)
+    assert model is not None
+
+
+#root endpoint
+@app.get("/")
+def root():
+    return dict(greeting="Bark!")
